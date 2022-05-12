cf8b7577954cd61cebdb0da359723ec179e74087
jona <iona.a.br@gmail.com>
2022-05-11 20:04:34 -0400

    segundo commit juego

--- js/juego.js
+++ js/juego.js
@@ -2,6 +2,7 @@ var nave;
 var balas;
 var timer = 0;
 var delay = 400;
+var aparecer;
 
 var Iniciar = {
     preload: function(){
@@ -33,7 +34,18 @@ var Iniciar = {
         balas.setAll('anchor.y', 1);
         balas.setAll('checkWorldBouds', true);
         balas.setAll('outOfBoundsKill', true);
-
+        //crear enemigos individuales
+        malos = juego.add.group();
+        malos.enableBody = true;
+        malos.setBodyType = Phaser.Physics.ARCADE;
+        malos.createMultiple(20, 'malo');
+        malos.setAll('anchor.x', 0.5);
+        malos.setAll('anchor.y', 1);
+        malos.setAll('checkWorldBounds', true);
+        malos.setAll('outOfBoundsKill', true);
+        //ciclo de enemigos
+        //loop(time,funcionloop)
+        aparecer = juego.time.events.loop(1500, this.crearEnemigo, this)
     },
 
     update: function() {
@@ -45,6 +57,9 @@ var Iniciar = {
         if(juego.imput.activePointer.isDown){
             this.disparar();
         }
+        //colision de rocas y balas
+        juego.physics.arcade.overlap(balas, malos, this.colision, null, this);
+        
     },
     //funcion disparar una sola bala
     disparar: function(){
@@ -57,5 +72,22 @@ var Iniciar = {
             juego.physics.arcade.angleToPointer(bala);
             juego.physics.arcade.moveToPointer(bala, 300);
         }
+    },
+    //funcion para la aparicion de enemigos
+    crearEnemigo: function(){
+        var enem = malos.getFirstDead();
+        var num = math.floor(Math.random() * 10 +1);
+        enem.reset(400, num * 55);
+        //enem.reset(num * 38, 0);
+        enem.anchor.setTo(0.5);
+        enem.body.velocity.x = -100;
+        enem.checkWorldBounds = true;
+        enem.outOfBoundsKill = true;
+
+    },
+    //funcion colision de balas y enemigos
+    colision: function(bala, malo){
+        bala.kill();
+        malo.kill();
     }
 };
